8235a40e6111a696e1c2a686ce3c65159158bca2
ShAiDSk <shaid.archlinux.coder.cp@gmail.com>
2023-01-18 11:23:10 +0530

    First Commit

---
+++ CP-Code/06-Dynamic-Programming/DP_With_priyansh/gpp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# CPP Exicutable file.
+g++ Day1-02.cpp -o Day1-02.out; ./Day1-02.out < in > out; cat cerr.txt; echo "\nOutput"; cat out
---
+++ Python/message.py
@@ -0,0 +1,3 @@
+import pywhatkit
+
+pywhatkit.sendwhatmsg("+916297540992", "Hi, Shaid this side!", 17, 36)
